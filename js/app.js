dba49385b9f66543a94597bf0d583c71d2afc394
Ekow Yawson <ekowyawson71@gmail.com>
2023-10-28 02:40:54 -0400

    Completing Lab 5 - question 5.

--- js/app.js
+++ js/app.js
@@ -105,12 +105,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+const getProduct = (a,b) => {return(a*b)};
 
+function multiplyArray(multArr) { //eslint-disable-line
+  // Calculate the product of the numbers in the array
+  const product = multArr.reduce((acc, current) => getProduct(acc, current), 1);
+
+  // Create the message string
+  const message = `The numbers ${multArr.join(',')} have a product of ${product}.`;
+
+  return [product, message];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
